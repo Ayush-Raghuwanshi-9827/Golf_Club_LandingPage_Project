493572aefb5db9eb9081e5760ac27c733c15b039
Ayush Raghuwanshi <raghuwanshiayush182@gmail.com>
2023-11-16 13:29:39 +0530

    This is golf club landing page project

---
+++ notes.js
@@ -0,0 +1,14 @@
+// difference BW var , let , cosnt
+
+// vr old js me tha = let const new js me hai
+// bar function scoped (var apne parent function me kahi bhi use ho sakts hai)= let braces scoped hota hai
+// var adds itself to the window object = let and const does not add itself to the window object
+// (js language me kuch cheeje nahi hai jo hum use kar sakte hai aur wo chheje hum js se nahi balki broser se milti hai,aise sarre features jo js ka part nahi hai but fir bhi use kar sakte hai unhe hmm dhoond sakte hai ek particular object meun jiska namm hai window)
+
+// Heap memory = jitne bhi intermediate data hum bannnate hai unhe store kahi to krna padta hai uske liye hota hai heap memory
+
+
+// excecution context ,mtlb jab hum function chalaayega fnc apna ek khudka ek imaginary container bana lega jisme uski cheeje hogi
+// 1 Variables
+// 2 functions inside that parent fnc
+// 3 lexical enviroment of that function
